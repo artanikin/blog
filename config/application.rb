892e8154b82f8af7babfef3e6624a0343a043011
Anikin Artyom <aanikin@naumen.ru>
2016-09-15 20:45:50 +0500

    I18n

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module Blog
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Ekaterinburg'
+    config.i18n.default_locale = :ru
   end
 end
